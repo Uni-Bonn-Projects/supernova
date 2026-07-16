96ce96787f235803a852ecb9d8e4116c0d1f49ea
dtiderko <dennis@tiderko.de>
2026-07-16 20:43:03 +0200

    csg: implement calculation of a csg interval

--- shaders/csg.glsl
+++ shaders/csg.glsl
@@ -1,10 +1,60 @@
 #include "common.glsl"
+#include "math.glsl"
+#line 5
+
 struct CSGInterval {
   float near;
   float far;
   vec3 normalNear;
   vec3 normalFar;
 };
+
+CSGInterval calcCSGInterval(
+  vec3 rayOrigin,
+  vec3 rayDir,
+  SNMesh mesh
+) {
+  CSGInterval result = CSGInterval(
+      Inf,
+      -1.0,
+      vec3(0.0),
+      vec3(0.0)
+    );
+
+  // for each triangle in current mesh
+  for (uint i = 0u; i < mesh.triangleCount; i++) {
+    // Fetch vertex positions
+    vec3 v0 = mesh.vertices[mesh.indices[i].x].xyz;
+    vec3 v1 = mesh.vertices[mesh.indices[i].y].xyz;
+    vec3 v2 = mesh.vertices[mesh.indices[i].z].xyz;
+
+    vec3 curResult = intersectTriangle(rayOrigin, rayDir, v0, v1, v2);
+
+    bool isNearer = curResult.z < result.near;
+    bool isFurther = curResult.z > result.far;
+    if (isNearer || isFurther) {
+      vec3 barycentrics = vec3(1.0 - curResult.x - curResult.y, curResult.xy);
+
+      // Fetch vertex normals
+      vec3 n0 = mesh.normals[mesh.indices[i].x].xyz;
+      vec3 n1 = mesh.normals[mesh.indices[i].y].xyz;
+      vec3 n2 = mesh.normals[mesh.indices[i].z].xyz;
+
+      vec3 normal = normalize(mat3(n0, n1, n2) * barycentrics);
+
+      if (isNearer) {
+        result.near = curResult.z;
+        result.normalNear = normal;
+      }
+      if (isFurther) {
+        result.far = curResult.z;
+        result.normalFar = normal;
+      }
+    }
+  }
+
+  return result;
+}
 
 RaytraceResult renderCSGInterval(
   vec3 rayOrigin,
--- shaders/raytracing.glsl
+++ shaders/raytracing.glsl
@@ -4,34 +4,6 @@
 #include "uniforms.glsl"
 #line 6
 
-vec3 intersectTriangle(
-  vec3 rayOrigin,
-  vec3 rayDir,
-  vec3 v0,
-  vec3 v1,
-  vec3 v2
-) {
-  // edges
-  vec3 e1 = v1 - v0;
-  vec3 e2 = v2 - v0;
-
-  // plane
-  vec3 n = cross(e1, e2);
-  vec3 c = v0;
-
-  // ray till triangle
-  float t = dot(n, c - rayOrigin) / dot(n, rayDir);
-  vec3 p = rayOrigin + t * rayDir - c;
-
-  float u = dot(cross(p, e2), n) / dot(n, n);
-  float v = dot(cross(e1, p), n) / dot(n, n);
-
-  if (u > 0 && v > 0 && u + v < 1 && t > 0) {
-    return vec3(u, v, t);
-  } else {
-    return vec3(Inf);
-  }
-}
 
 /// We are only using triangles to render OLDMAN. Therefore, this function
 /// raytraces all triangle stuff with some values, like color, hardcoded for
